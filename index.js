549aa60a6eb16b18bf86e4946266108aa027ce8a
Mesut Biskin <msbiskin@gmail.com>
2023-10-05 15:48:03 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const nums = [3, 5, 7, 2]// this is an array of numbers === This is clean code
+const nums2 =[3,4, 6, 4] // this is also an array of nubmers === This is not clean code
+
+// console.log(nums[2])  // This will output the number 7 
+
+let fruits = ['banana', 'apple', 'kivi'] // this is an array strings
+
+console.log(fruits[1]) // this will output apple
+
+fruits[0] = "pinapple"  // Change the value of the first item to 'pinapple.This will replace the banana with pinapple
+
+console.log(fruits[0]) // This will output pineapple
+
+let things = [6, "todd", false, "false", [2]] // This array has different data types in it
+
+console.log(things[4])
